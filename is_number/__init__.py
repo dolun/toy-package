b99cdd0947667a2a163d7bbb3e5f4a15992cd675
thomas.dautremer <thomas.dautremer@cea.fr>
2022-01-31 15:49:17 +0100

    fix bug

--- is_number/__init__.py
+++ is_number/__init__.py
@@ -1,5 +1,5 @@
 """Utility functions to calculate if an object is a number and other things."""
-from .core import is_it_number, is_float, add_numbers
+from .core import is_it_number, is_float, add_numbers, get_array_shape
 # from module1 import square_plus_one
 # from ._version import get_versions
 
